ddfb9c88f7d3e5a41c23a7276794348889203985
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-09-19 20:45:55 -0300

    Add files via upload

---
+++ 1184 - Abaixo da Diagonal Principal.js
@@ -0,0 +1,36 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var dados = input.split('\n');
+
+
+let opr = dados.shift();
+let matriz=[];
+matriz.length = 12;
+let soma=0;
+let media =0;
+
+for(let i=0; i<12; i++){
+    matriz[i]= new Array(3);
+    for(let j=0; j<12; j++){
+        matriz[i][j]=parseFloat(dados.shift());
+    }
+}
+for(let i=1; i<12; i++){
+    for(let j=0; j<11; j++){
+        if(i>j){
+            soma+=matriz[i][j];
+        }
+
+    }
+    
+}
+
+media = (soma/66);
+
+
+if(opr.startsWith("S")){
+    console.log(soma.toFixed(1));
+    
+}else{
+    console.log(media.toFixed(1));
+    
+}
