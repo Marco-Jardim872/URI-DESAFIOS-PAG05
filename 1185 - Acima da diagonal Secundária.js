8a24384882c58126e9dd09b0bffa2e4ed22f0f5e
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-09-19 20:46:06 -0300

    Add files via upload

---
+++ 1185 - Acima da diagonal Secundária.js
@@ -0,0 +1,36 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var dados = input.split('\n');
+
+
+let opr = dados.shift();
+let matriz=[];
+matriz.length = 12;
+let soma=0;
+let media =0;
+
+for(let i=0; i<12; i++){
+    matriz[i]= new Array(3);
+    for(let j=0; j<12; j++){
+        matriz[i][j]=parseFloat(dados.shift());
+    }
+}
+for(let i=0; i<11; i++){
+    for(let j=10; j>=0; j--){
+        if((i+j)<11){
+            soma+=matriz[i][j];
+    }
+
+    }
+    
+}
+
+media = (soma/66);
+
+
+if(opr.startsWith("S")){
+    console.log(soma.toFixed(1));
+    
+}else{
+    console.log(media.toFixed(1));
+    
+}
