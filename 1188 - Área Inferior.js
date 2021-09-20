c05683f00701467cd6311f7baa477e65727abd0b
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-09-20 12:40:39 -0300

    Add files via upload

---
+++ 1188 - Área Inferior.js
@@ -0,0 +1,35 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var dados = input.split('\n');
+
+let opr = dados.shift();
+let matriz=[];
+matriz.length = 12;
+let soma=0;
+let media =0;
+let count=0;
+
+for(let i=0; i<12; i++){
+    matriz[i]= new Array(12);
+    for(let j=0; j<12; j++){
+        matriz[i][j]=parseFloat(dados.shift());
+    }
+}
+
+
+for(let i=11; i>6; i--){
+    for(let j = 10 - count ; j>0 + count; j--){
+        
+        soma+= matriz[i][j];
+       
+    }
+    count++;
+}
+
+
+media = (soma/30);
+
+if(opr.startsWith("S")){
+    console.log(soma.toFixed(1));
+}else{
+    console.log(media.toFixed(1));
+}
